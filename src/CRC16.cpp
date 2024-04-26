ff50db7f3c90f5e6e4123b6151b7911a1dadccb5
thisdp <30386052+thisdp@users.noreply.github.com>
2024-04-26 15:41:52 +0800

    fix no cache

--- src/CRC16.cpp
+++ src/CRC16.cpp
@@ -1,83 +1,35 @@
 #include "CRC16.h"
 
-#ifndef CRC16NoCache
-/*Reflect Cache Calculations*/
-uint8_t reflectLookupTable[] = {
-  0,128,64,192,32,160,96,224,16,144,80,208,48,176,112,240,
-  8,136,72,200,40,168,104,232,24,152,88,216,56,184,120,248,
-  4,132,68,196,36,164,100,228,20,148,84,212,52,180,116,244,
-  12,140,76,204,44,172,108,236,28,156,92,220,60,188,124,252,
-  2,130,66,194,34,162,98,226,18,146,82,210,50,178,114,242,
-  10,138,74,202,42,170,106,234,26,154,90,218,58,186,122,250,
-  6,134,70,198,38,166,102,230,22,150,86,214,54,182,118,246,
-  14,142,78,206,46,174,110,238,30,158,94,222,62,190,126,254,
-  1,129,65,193,33,161,97,225,17,145,81,209,49,177,113,241,
-  9,137,73,201,41,169,105,233,25,153,89,217,57,185,121,249,
-  5,133,69,197,37,165,101,229,21,149,85,213,53,181,117,245,
-  13,141,77,205,45,173,109,237,29,157,93,221,61,189,125,253,
-  3,131,67,195,35,163,99,227,19,147,83,211,51,179,115,243,
-  11,139,75,203,43,171,107,235,27,155,91,219,59,187,123,251,
-  7,135,71,199,39,167,103,231,23,151,87,215,55,183,119,
-  247,15,143,79,207,47,175,111,239,31,159,95,223,63,191,127,255,
-};
-
-uint8_t CRC16::reflectFast(uint8_t data){
-  return reflectLookupTable[data];
-}
-
 typedef struct uint16_fast {
   uint8_t low;
   uint8_t high;
 }uint16_f;
 
+uint16_t *CRC16ReflectCache = 0;
+/*Reflect Cache Calculations*/
+void CRC16GenerateReflectCache(){
+  if(!CRC16ReflectCache){
+    CRC16ReflectCache = new uint16_t[256];
+    for (int i = 0; i < 256; i++) {
+      CRC16ReflectCache[i] = (i * 0x0202020202ULL & 0x010884422010ULL) % 1023;
+    }
+  }
+}
+
+uint8_t CRC16::reflectFast(uint8_t data){
+  return CRC16ReflectCache[data];
+}
+
 uint16_t CRC16::reflectFast(uint16_t data){
-  return reflectLookupTable[((uint16_f*)&data)->high] | (reflectLookupTable[((uint16_f*)&data)->low] << 8);
-}
-
-void CRC16::updateFast(uint8_t data) {
-  data = reflectIn ? reflect(data) : data;
-  crc = (crc << 8) ^ crcTable[(crc >> 8) ^ data];
-}
-
-void CRC16::updateFast(uint8_t *data, uint32_t len){
-  for(uint32_t i=0; i<len; i++)
-    update(data[i]);
-}
-#endif
-
-
-uint8_t CRC16::reflect(uint8_t data){
-#ifndef CRC16NoCache
-  return reflectFast(data);
-#else
-  return reflectRaw(data);
-#endif
-}
-
-uint16_t CRC16::reflect(uint16_t data){
-#ifndef CRC16NoCache
-  return reflectFast(data);
-#else
-  return reflectRaw(data);
-#endif
+  return CRC16ReflectCache[((uint16_f*)&data)->high] | (CRC16ReflectCache[((uint16_f*)&data)->low] << 8);
 }
 
 uint8_t CRC16::reflectRaw(uint8_t data){
-  uint8_t reflection = 0x00;
-  for (int8_t bit = 7; bit >= 0; bit--){
-      reflection |= (data & 0x01) << bit;
-      data >>= 1;
-  }
-  return reflection;
+  return (data * 0x0202020202ULL & 0x010884422010ULL) % 1023;
 }
 
 uint16_t CRC16::reflectRaw(uint16_t data){
-  uint16_t reflection = 0x0000;
-  for (int8_t bit = 15; bit >= 0; bit--){
-    reflection |= (data & 0x01) << bit;
-    data >>= 1;
-  }
-  return reflection;
+  return ((((uint16_f*)&data)->high * 0x0202020202ULL & 0x010884422010ULL) % 1023)|(((((uint16_f*)&data)->low * 0x0202020202ULL & 0x010884422010ULL) % 1023)<<8);
 }
 
 /*CRC Calculations*/
@@ -88,20 +40,14 @@ uint16_t CRC16::calc(uint8_t data) {
   return tCRC;
 }
 
-void CRC16::update(uint8_t data) {
-#ifndef CRC16NoCache
-  return updateFast(data);
-#else
-  return updateRaw(data);
-#endif
+void CRC16::updateFast(uint8_t data) {
+  data = reflectIn ? reflect(data) : data;
+  crc = (crc << 8) ^ CRC16CRCCache[(crc >> 8) ^ data];
 }
 
-void CRC16::update(uint8_t *data, uint32_t len){
-#ifndef CRC16NoCache
-  return updateFast(data,len);
-#else
-  return updateRaw(data,len);
-#endif
+void CRC16::updateFast(uint8_t *data, uint32_t len){
+  for(uint32_t i=0; i<len; i++)
+    reflect(data[i]);
 }
 
 void CRC16::updateRaw(uint8_t data) {
@@ -116,25 +62,42 @@ void CRC16::updateRaw(uint8_t *data, uint32_t len){
 }
 
 /*CRC Operations*/
-CRC16::CRC16(CRC16Model cfg){
-  config = cfg;
-  crc = xorIn;
-  genCRCTable();
+CRC16::~CRC16(){
+  if(CRC16CRCCache) delete CRC16CRCCache;
 }
 
-CRC16::CRC16(uint8_t _reflectIn, uint8_t _reflectOut, uint16_t _polynomial, uint16_t _xorIn, uint16_t _xorOut){
+CRC16::CRC16(CRC16Model cfg, bool useCache){
+  config = cfg;
+  crc = xorIn;
+  CRC16CRCCache = 0;
+  CRCUseCache = useCache;
+  if(useCache){
+    CRC16GenerateReflectCache();
+    CRC16CRCCache = new uint16_t[256];
+    for(uint16_t i=0; i<=255; i++) CRC16CRCCache[i] = calc(i);
+  }
+}
+
+CRC16::CRC16(uint8_t _reflectIn, uint8_t _reflectOut, uint16_t _polynomial, uint16_t _xorIn, uint16_t _xorOut, bool useCache){
   reflectIn = _reflectIn;
   reflectOut = _reflectOut;
   polynomial = _polynomial;
   xorIn = _xorIn;
   xorOut = _xorOut;
   crc = xorIn;
-  genCRCTable();
+  CRCUseCache = useCache;
+  if(useCache){
+    CRC16GenerateReflectCache();
+    CRC16CRCCache = new uint16_t[256];
+    for(uint16_t i=0; i<=255; i++) CRC16CRCCache[i] = calc(i);
+  }
 }
 
 void CRC16::setModel(CRC16Model cfg){
   config = cfg;
-  genCRCTable();
+  if(CRCUseCache){
+    for(uint16_t i=0; i<=255; i++) CRC16CRCCache[i] = calc(i);
+  }
 }
 
 void CRC16::clear(){
@@ -145,9 +108,26 @@ uint16_t CRC16::get(){
   return (reflectOut?reflect(crc):crc)^xorOut;
 }
 
-void CRC16::genCRCTable(){
-  for(uint16_t i=0; i<=255; i++)
-    crcTable[i] = calc(i);
+void CRC16::update(uint8_t data){
+  if(CRCUseCache)
+    updateFast(data);
+  else
+    updateRaw(data);
+}
+
+void CRC16::update(uint8_t *data, uint32_t length){
+  if(CRCUseCache)
+    updateFast(data, length);
+  else
+    updateRaw(data, length);
+}
+
+uint8_t CRC16::reflect(uint8_t data){
+  return CRCUseCache?reflectFast(data):reflectRaw(data);
+}
+
+uint16_t CRC16::reflect(uint16_t data){
+  return CRCUseCache?reflectFast(data):reflectRaw(data);
 }
 
 /*CRC16 Models*/
--- src/CRC16.h
+++ src/CRC16.h
@@ -20,31 +20,32 @@ extern CRC16Model CRC16X25;
 extern CRC16Model CRC16XMODEM;
 extern CRC16Model CRC16DNP;
 
+extern uint16_t *CRC16ReflectCache;
+
 class CRC16{
 public:
-  CRC16(CRC16Model cfg);
-  CRC16(uint8_t reflectIn, uint8_t reflectOut, uint16_t polynomial, uint16_t xorIn, uint16_t xorOut);
+  CRC16(CRC16Model cfg, bool useCache = true);
+  ~CRC16();
+  CRC16(uint8_t reflectIn, uint8_t reflectOut, uint16_t polynomial, uint16_t xorIn, uint16_t xorOut, bool useCache = true);
   void setModel(CRC16Model cfg);
-  void update(uint8_t data);
-  void update(uint8_t *data, uint32_t len);
   void updateRaw(uint8_t data);
   void updateRaw(uint8_t *data, uint32_t len);
-  uint16_t get();
-  void clear();
-private:
-  #ifndef CRC16NoCache
-  uint16_t crcTable[256];
-  void updateFast(uint8_t data);
-  void updateFast(uint8_t *data, uint32_t len);
-  uint8_t reflectFast(uint8_t data);
-  uint16_t reflectFast(uint16_t data);
-  #endif
+  void update(uint8_t data);
+  void update(uint8_t *data, uint32_t len);
   uint8_t reflectRaw(uint8_t data);
   uint16_t reflectRaw(uint16_t data);
   uint8_t reflect(uint8_t data);
   uint16_t reflect(uint16_t data);
+  uint16_t get();
+  void clear();
+private:
+  uint16_t *CRC16CRCCache;
+  bool CRCUseCache;
+  void updateFast(uint8_t data);
+  void updateFast(uint8_t *data, uint32_t len);
+  uint8_t reflectFast(uint8_t data);
+  uint16_t reflectFast(uint16_t data);
   uint16_t calc(uint8_t data);
-  void genCRCTable();
   union{
     CRC16Model config;
     struct{
